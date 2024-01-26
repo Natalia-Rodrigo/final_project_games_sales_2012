914ac8c388833a5dbf5fd8d51200da07a6cfe04c
natalia <nataliarodrigo18@gmail.com>
2024-01-26 19:11:36 +0000

    updated functions file

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def update_year(df: pd.DataFrame="video_games_df", indices: list=None, year: float=None):
     """
@@ -20,3 +23,20 @@ def rename_columns(df: pd.DataFrame):
     - df (pd.DataFrame): The DataFrame with columns to be renamed.
     """
     df.columns = df.columns.str.lower().str.replace(' ', '_')
+
+def countplot_categorical_variables(df: pd.DataFrame, figsize_height: int = 16, figsize_width: int = 16):
+    """This functions takes as:
+    Input: a df with categorical variables, and figsizes with a default value each of 16
+    Output: countplot for each column of the df sorted by frequency. Whenever the nº unique values > 6, bars will be placed in the y-axis"""
+
+    for col in df:
+        if len(df[col].unique()) >= 6:
+            fig, ax = plt.subplots(figsize=(figsize_width, figsize_height))
+            sns.countplot(data=df, y=col, order=df[col].value_counts().index, ax=ax)
+            ax.set_title(f'Countplot for {col}')
+        else:
+            fig, ax = plt.subplots(figsize=(figsize_width, figsize_height))
+            sns.countplot(data=df, x=col, order=df[col].value_counts().index, ax=ax)
+            ax.set_title(f'Countplot for {col}')
+
+        plt.show()
